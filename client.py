6fac74397fcc750e68dc24ffda3ea689938a38bb
Jesse Rengers <j.j.rengers@student.utwente.nl>
2021-12-28 19:04:51 +0100

    added more options to the client and a default one for testing

--- client.py
+++ client.py
@@ -10,10 +10,15 @@ import requests
 from icmplib import ping
             
 #instruct flooder to start flooding <address> for <duration> seconds after <offset> seconds
-def instruct_flooder(address,offset, duration):
-    url = 'https://'+address+'/flooder/'
-    myobj = {'key': 'ditiseensoortwachtwoord', 'offset': offset, 'duration':duration}
-    x = requests.post(url, data = myobj)
+def instruct_flooder(address,offset, duration,sink_ip,sink_port,packet_size):
+    url = 'http://'+address+':3000/flooder/'
+    params = {"key": "ditiseensoortwachtwoord",
+        "offset": offset,
+        "duration":duration,
+        "sink_ip": sink_ip,
+        "sink_port": sink_port,
+        "packet_size": packet_size}
+    x = requests.post(url, data = params)
     assert x.status_code == 200, "flooder can not be reached"
  
 # Measure RTT using ICMP of the Server. Data gets stored in a pre-defined csv file
@@ -44,7 +49,7 @@ def validate_ip(s):
 def main():
     serverIP = ''
     flooderIP = ''
-    print('Usage: start <server IP> <flooder IP>')
+    print('Usage: start <server IP> <flooder IP> <packet sink IP> <packet sink port> <packet length> <duration> <offset>')
     while True:
         x = input(">>>")
         parsed = x.split()
@@ -52,15 +57,24 @@ def main():
             break
         if parsed[0] == 'help':
             print('Usage: start <server IP> <flooder IP>')
+        if parsed[0] == 'default':
+            print('using default values')
+            instruct_flooder('127.0.0.1',3,5,'192.168.2.13',161,50)
+            get_rtt('1.1.1.1',8)
         if parsed[0] == 'start':
-            if len(parsed) == 3:
-                if validate_ip(parsed[1]) & validate_ip(parsed[2]): 
+            if len(parsed) == 8:
+                if validate_ip(parsed[1]) & validate_ip(parsed[2]) & validate_ip(parsed[3]): 
                     serverIP = parsed[1]
                     flooderIP = parsed[2]
+                    packetSinkIP = parsed[3]
+                    packetSinkPort = parsed[4]
+                    packetLength = parsed[5]
+                    duration = parsed[6]
+                    offset = parsed[7]
 
-                    #instruct flooder to flood for 2 seconds after 3 seconds
-                    instruct_flooder(flooderIP,3,2)
-                    get_rtt(serverIP,7)
+                    #instruct flooder to flood for 10 seconds after 2 seconds
+                    instruct_flooder(flooderIP,offset,duration,packetSinkIP,packetSinkPort,packetLength)
+                    get_rtt(serverIP,duration + 2*offset)
 
 if __name__ == "__main__":
     main()
